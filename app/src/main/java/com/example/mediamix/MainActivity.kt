f8863a82a0053b623d6f2bd8ec4f981f5986d815
Alejandro <asanlan@g.educaand.es>
2025-02-19 10:16:23 +0100

    Estructura Inicial Completada

--- app/src/main/java/com/example/mediamix/MainActivity.kt
+++ app/src/main/java/com/example/mediamix/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mediamix
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.mediamix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         // Inicializar View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Aplicar ViewCompat para manejar los insets y evitar que la UI se desborde
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.root.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // Cargar el fragmento por defecto (Biblioteca)
         cambiarFragmento(FragmentoBiblioteca())
